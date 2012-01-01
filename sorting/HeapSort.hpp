8f38a14793ae62aa7d02c51f302ce8bf98d21dc8
wush978 <wush978@gmail.com>
2012-01-02 01:01:35 +0800

    Sorting, chapter 5

---
+++ sorting/HeapSort.hpp
@@ -0,0 +1,95 @@
+/*
+ * HeapSort.hpp
+ *
+ *  Created on: 2012/1/1
+ *      Author: wush
+ */
+
+#ifndef HEAPSORT_HPP_
+#define HEAPSORT_HPP_
+
+namespace wush
+{
+
+	namespace sorting
+	{
+		typedef unsigned int Count;
+
+		/**
+		 * See page 112 of Corment
+		 */
+		inline const Count Parent(const Count i) { return (i+1)/2 - 1; }
+		inline const Count Left(const Count i) { return 2*(i+1) - 1; }
+		inline const Count Right(const Count i) { return 2*(i+1); }
+
+
+		/**
+		 * See page 115 of Cormen
+		 * MaxHeapify assumed that the binary trees rooted at LEFT(i) and RIGHT(i) are max-heaps, but
+		 * that A[i] may be smaller than its children, thus violating the max-heap property.
+		 * MaxHeapify will correct this violation.
+		 *
+		 * @params A 	container with operator [] and <
+		 */
+		// parent(i) = (i+1)/2 - 1
+		// left(i) = 2*(i+1) - 1
+		// right(i) = 2*(i+1)
+		template<class Array>
+		void MaxHeapify(Array& A, Count i, const Count A_size)
+		{
+			bool is_continue = true;
+			while (is_continue) {
+				is_continue = true;
+				Count
+					l = 2(i+1) - 1,
+					r = 2*(i+1),
+					largest;
+				if (l < A_size && A[l] > A[i]) {
+					largest = l;
+				}
+				else {
+					largest = i;
+				}
+				if (r < A_size && A[r] > A[largest]) {
+					largest = r;
+				}
+				if (largest != i) {
+					Count temp(A[i]);
+					A[i] = A[largest];
+					A[largest] = temp;
+					i = largest;
+				}
+				else {
+					is_continue = false;
+				}
+			}
+		}
+
+		template<class Array>
+		void BuildMaxHeap(Array& A, const Count A_size) {
+			bool is_continue = true;
+			while (is_continue) {
+				Count i = A_size/2;
+				while (i >= 1) {
+					i--;
+					MaxHeapify<Array>(A, i, A_size);
+				}
+			}
+		}
+
+		template<class Array>
+		void HeapSort(Array& A, Count A_size)
+		{
+			BuildMaxHeap<Array>(A, A_size);
+			for (Count i= A_size - 1;i >= 1;i--) {
+				Count temp(A[0]);
+				A[0] = A[i];
+				A[i] = temp;
+				A_size--;
+				MaxHeapify(A, 1);
+			}
+		}
+	}
+}
+
+#endif /* HEAPSORT_HPP_ */
